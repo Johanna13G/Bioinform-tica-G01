fa13909c4a660a7f73b54a1cca8a42afeec99aa8
Johanna13G <94419967+Johanna13G@users.noreply.github.com>
2021-12-09 20:26:02 -0500

    Update Netsize.sh
    Determinación del número de filas

--- Deber 1/Netsize.sh
+++ Deber 1/Netsize.sh
@@ -6,3 +6,13 @@ se utiliza el comando cd, en mi caso primero utilizé pwd para saber en qué
 directorio me encontraba y obtuve lo siguiente:
 pwd
 #/c/Users/JOHANNA
+a partir de lo anterior utilicé el comando cd hasta ubicar la carpeta Saavedra2013 que 
+contiene los datos que se necesitarán para la resolución del ejercicio.
+Una vez ubicada la carpeta elegimos un archivo del cual se van a determinar el número
+de filas y columnas. Se eligió el archivo n1.txt
+FILAS
+Para determinar el número de filas utilizamos el siguiente comando wc -l el cual 
+sirve para contar líneas y esto a su vez es igual que filas. Entonces:
+wc -l n1.txt
+Se obtiene
+97 n1.txt  por lo tanto hay 97 filas en el archivo n1.txt 
