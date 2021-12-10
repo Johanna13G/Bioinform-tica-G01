c313755f0ad0ad3999de72596a5b9af19304fd7f
Johanna13G <94419967+Johanna13G@users.noreply.github.com>
2021-12-09 20:23:12 -0500

    Create Netsize.sh
    primer paso para llamar a la carpeta

---
+++ Deber 1/Netsize.sh
@@ -0,0 +1,8 @@
+1. Escriba un guión que tome uno de estos archivos y determine el número de filas (polinizadores) 
+y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un 
+espacio al final de cada línea. Tu guión debería regresar
+Lo primero que se debe hacer es ubicar la carpeta que contiene los datos y para esto
+se utiliza el comando cd, en mi caso primero utilizé pwd para saber en qué 
+directorio me encontraba y obtuve lo siguiente:
+pwd
+#/c/Users/JOHANNA
