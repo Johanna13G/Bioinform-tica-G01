e6981e4f95116ab9de24f330e21053d47fa824ab
Johanna13G <94419967+Johanna13G@users.noreply.github.com>
2021-12-09 20:59:16 -0500

    Create Netsize_all.sh
    Especificaciones de los comandos que se necesitan para resolver el literal 2

---
+++ Deber 1/Netsize_all.sh
@@ -0,0 +1,4 @@
+2. Escriba un guión que imprima el número de filas y columnas para cada red:
+Para esto llamaremos a la carpeta la cual contiene los datos de ejercicio utilizando el comando
+for, luego añadimos el comando do indicando lo que queremos hacer, luego el comando wc -l el cual 
+indica que se debe contar el número de filas
